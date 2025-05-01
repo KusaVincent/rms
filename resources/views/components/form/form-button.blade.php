77b9d09a3613dfca8fc124378337f1d6f1c1a5fe
KusaVincent <vincentkusa170203@gmail.com>
2025-05-01 18:44:47 +0300

    Customer Support via support page

--- resources/views/components/form/form-button.blade.php
+++ resources/views/components/form/form-button.blade.php
@@ -2,6 +2,6 @@
     type="submit"
     class="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 flex items-center justify-center"
     wire:loading.attr="disabled">
-    <x-ui.loader-spinner />
     <span wire:loading.class="hidden">Send Message</span>
+    <x-ui.loader-spinner />
 </button>
