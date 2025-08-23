befd22afa2f7e8e71253edaf495fc3d088b2993f
Vincent Kusa <vincentkusa170203@gmail.com>
2025-08-23 17:52:09 +0300

    Filter enhancenced

--- app/View/Components/Negotiable.php
+++ app/View/Components/Negotiable.php
@@ -10,18 +10,7 @@ use Illuminate\View\Component;
 
 final class Negotiable extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
+    public function render(): View
     {
         return view('components.negotiable');
     }
--- resources/views/components/negotiable.blade.php
+++ resources/views/components/negotiable.blade.php
@@ -1,5 +1,7 @@
 @props(['negotiable'])
 
 @if($negotiable)
-    <span class="italic text-blue-500">{{ ('Negotiable') }}</span>
+    <span class="italic inline-block ml-1 px-2 py-0.5 text-xs font-semibold text-blue-700 bg-blue-100 rounded">
+        {{ __('Negotiable') }}
+    </span>
 @endif
